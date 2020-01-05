1e519114d004521a7ee9e6ac8e8008f110ce781e
MJX99 <399895708@qq.com>
2020-01-05 10:33:56 +0800

    上传

---
+++ src/main/java/xyz/lightseekers/maven_blog/service/IVisitorService.java
@@ -0,0 +1,22 @@
+package xyz.lightseekers.maven_blog.service;
+
+import xyz.lightseekers.maven_blog.bean.Visitor;
+
+
+import java.util.List;
+
+public interface IVisitorService {
+
+    List<Visitor> selectAllVisitor() throws RuntimeException;
+
+    List<Visitor> selectAllByURl(String url) throws RuntimeException;
+
+    List<Visitor> selectAllByIp(String ip) throws RuntimeException;
+
+    int deleteById(int id) throws RuntimeException;
+
+    int insert( Visitor visitor ) throws RuntimeException;
+
+    int update( Visitor visitor) throws RuntimeException;
+
+}
